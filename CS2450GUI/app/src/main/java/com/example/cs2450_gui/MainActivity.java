ac809da920a0d88adfb2fe368dfd8339b4465707
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-11-09 12:20:26 -0800

    added .gitignore file. created android studio project.

---
+++ CS2450GUI/app/src/main/java/com/example/cs2450_gui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cs2450_gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
